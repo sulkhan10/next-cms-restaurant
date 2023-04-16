05e7ab19f34761ccc88f094c2ecf7c670823540f
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-04-16 18:26:46 +0700

    feat : initialize project

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/admin/dashboard">Dashboard</Link>
+    </main>
+  );
+}
