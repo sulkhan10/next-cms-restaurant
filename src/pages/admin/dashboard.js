d503bea0ef2899794b56e95a57d04606747d74dc
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-04-17 11:10:48 +0700

    feat : add cards in aside table

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -10,6 +10,7 @@ import {
   FiEye,
 } from "react-icons/fi";
 import { FaSort } from "react-icons/fa";
+import Image from "next/image";
 export default function Dashboard() {
   const customerData = [
     {
@@ -80,42 +81,60 @@ export default function Dashboard() {
         <Header />
         <div className="flex px-5 justify-center items-center h-[90vh] gap-4">
           <div className="w-4/5 bg-gray-50 h-full ">
-            <div className=" p-5 h-48 bg-[#5D5FEF] rounded-3xl flex flex-col justify-between text-white">
-              <h1 className="text-3xl font-semibold mb-2">Customer</h1>
-              <div className="">
-                <h1 className="text-xl">
-                  On this menu you will be able to create, edit, and also delete
-                </h1>
-                <h1 className="text-xl mb-1">
-                  the customer. Also you can manage it easily.
-                </h1>
+            <div className="relative p-5 h-48 bg-[#5D5FEF] rounded-3xl flex flex-col justify-between text-white">
+              <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-6/12 lg:max-w-full lg:absolute xl:px-0 filter brightness-50">
+                <svg
+                  className="absolute left-0 hidden h-full  transform -translate-x-1/2 lg:block"
+                  viewBox="0 0 100 100"
+                  fill="#7D7FFF"
+                  preserveAspectRatio="none slice"
+                >
+                  <path d="M50 0H100L50 100H0L50 0Z" />
+                </svg>
+                <img
+                  className="object-cover w-full h-56  shadow-lg  lg:shadow-none md:h-96 lg:h-full rounded-3xl"
+                  src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                  alt=""
+                />
               </div>
-              <div className="flex gap-4 justify-between">
-                <button className="bg-white bg-opacity-20  px-5 py-2 rounded-lg flex items-center w-1/5 justify-center">
-                  <span className="mr-2 text-2xl">+</span>Add New Customer
-                </button>
-                <form className="flex flex-grow justify-between gap-2 items-center pl-4 bg-gray-50 rounded-xl p-1 w-2/5">
-                  <FiSearch size={25} color="#D1D0D3" />
-                  <input
-                    type="text"
-                    className="  bg-gray-50  w-full px-2 py-1 text-gray-800"
-                    placeholder="Search Customer"
-                  />
-                  <button className="bg-[#5D5FEF] px-5 py-2 rounded-lg">
-                    Search
+              <div className="relative ">
+                <h1 className="text-3xl font-semibold mb-2">Customer</h1>
+                <div className="">
+                  <h1 className="text-xl">
+                    On this menu you will be able to create, edit, and also
+                    delete
+                  </h1>
+                  <h1 className="text-xl mb-1">
+                    the customer. Also you can manage it easily.
+                  </h1>
+                </div>
+                <div className="flex gap-4 justify-between">
+                  <button className="bg-white bg-opacity-20  px-5 py-2 rounded-lg flex items-center w-1/5 justify-center">
+                    <span className="mr-2 text-2xl">+</span>Add New Customer
                   </button>
-                </form>
-                <button className="bg-white bg-opacity-20 flex gap-2 items-center px-5 py-2 rounded-lg w-1/8">
-                  <FiFilter size={25} />
-                  Filter
-                </button>
-                <button className="bg-white bg-opacity-20 flex gap-2 items-center px-5 py-2 rounded-lg w-1/8">
-                  <FiRefreshCw size={25} />
-                  Refresh
-                </button>
-                <button className="bg-white bg-opacity-20  px-5 py-2 rounded-lg w-1/20">
-                  <FiPrinter size={25} />
-                </button>
+                  <form className="flex flex-grow justify-between gap-2 items-center pl-4 bg-gray-50 rounded-xl p-1 w-2/5">
+                    <FiSearch size={25} color="#D1D0D3" />
+                    <input
+                      type="text"
+                      className="  bg-gray-50  w-full px-2 py-1 text-gray-800"
+                      placeholder="Search Customer"
+                    />
+                    <button className="bg-[#5D5FEF] px-5 py-2 rounded-lg">
+                      Search
+                    </button>
+                  </form>
+                  <button className="bg-white bg-opacity-20 backdrop-blur-sm flex gap-2 items-center px-5 py-2 rounded-lg w-1/8">
+                    <FiFilter size={25} />
+                    Filter
+                  </button>
+                  <button className="bg-white bg-opacity-20 backdrop-blur-sm flex gap-2 items-center px-5 py-2 rounded-lg w-1/8">
+                    <FiRefreshCw size={25} />
+                    Refresh
+                  </button>
+                  <button className="bg-white bg-opacity-20 backdrop-blur-sm px-5 py-2 rounded-lg w-1/20">
+                    <FiPrinter size={25} />
+                  </button>
+                </div>
               </div>
             </div>
             <table className="w-full border-collapse  text-gray-600 mt-4 ">
@@ -221,33 +240,68 @@ export default function Dashboard() {
               <div className="flex justify-between items-center">
                 <h1 className="text-gray-600">Showing 10 Data Customers</h1>
                 <div className="flex gap-2 text-gray-600 font-semibold">
-                <button className="bg-white px-5 py-1 rounded-lg text-black shadow-md transform rotate-1">
-  1
-</button>
-
-                  <button className=" px-5 py-2 rounded-lg">
-                    2
+                  <button className="bg-white px-5 py-1 rounded-lg text-black shadow-md transform rotate-1">
+                    1
                   </button>
-                  <button className="px-5 py-2 rounded-lg">
-                    3
-                  </button>
-                  <button className="px-5 py-2 rounded-lg">
-                    . . .
-                  </button>
-                  <button className="px-5 py-2 rounded-lg">
-                    38
-                  </button>
-                  <button className="px-5 py-2 rounded-lg">
-                    Next
-                  </button>
+                  <button className=" px-5 py-2 rounded-lg">2</button>
+                  <button className="px-5 py-2 rounded-lg">3</button>
+                  <button className="px-5 py-2 rounded-lg">. . .</button>
+                  <button className="px-5 py-2 rounded-lg">38</button>
+                  <button className="px-5 py-2 rounded-lg">Next</button>
                 </div>
               </div>
             </div>
-            <div className="h-8">
+            <div className="h-8"></div>
+          </div>
+          <div className="w-1/5  bg-gray-50 h-full pr-4 ">
+            <div className="bg-[#5D5FEF] h-64 p-5 rounded-3xl flex flex-col justify-between relative">
+              <div>
+                <h1 className="text-2xl">See analytics of</h1>
+                <h1 className="text-2xl">the Customer</h1>
+                <h1 className="text-2xl">Clearly</h1>
+              </div>
+              <button className="bg-white z-50 bg-opacity-20 backdrop-blur-sm flex gap-2 items-center justify-center py-4 rounded-lg w-32 ">
+                See Analitycs
+              </button>
+              <div className="absolute bottom-0 right-0">
+                <div className="bg-[#5D5FEF] border-[#A5A6F6] border-2 absolute bottom-0 right-0 rounded-tl-full w-32 h-32 rounded-br-3xl"></div>
+              </div>
+              <div className="absolute bottom-0 right-0">
+                <div className="bg-[#7879F1] border-[#F2F2F2] border-2 absolute bottom-0 right-0 rounded-tl-full w-20 h-20 rounded-br-3xl"></div>
+              </div>
+              <div className="absolute bottom-0 right-0">
+                <div className="bg-[#A5A6F6] border-[#F2F2F2] border-2 absolute bottom-0 right-0 rounded-tl-full rounded-br-3xl w-10 h-10"></div>
+              </div>
+            </div>
+            <div className=" bg-gray-100 mt-6 rounded-3xl  text-gray-800">
+                <div className="py-5 px-4">
 
+              <div className="text-2xl font-semibold">Top Menu</div>
+              <div className="text-2xl font-semibold text-[#F17300]">
+                This Week
+              </div>
+              <div className="text-sm py-5">10 - 12 Agustus 2023</div>
+              <div className="relative">
+                <div className="px-4 py-4 bg-white shadow-md font-semibold text-sm rounded-lg  transform rotate-1">
+                  Nasi Goreng Jamur Special Resto Pak Min
+                </div>
+                <div className="absolute -top-2 -right-3 mt-1 mr-1 bg-gray-700 text-gray-50 font-semibold text-xs  w-6 h-6 flex items-center justify-center rotate-12">
+                  1
+                </div>
+                <div className="absolute -top-3 -right-2 mt-1 mr-1 bg-[#F17300] text-gray-50 font-semibold text-xs  w-6 h-6 flex items-center justify-center rotate-12">
+                  1
+                </div>
+              </div>
+              <div className="text-sm py-4">2. Tongseng Sapi Gurih</div>
+              <div className="text-sm py-4">3. Nasi Gudeg Telur Ceker</div>
+              <div className="text-sm py-4">4. Nasi Ayam serundeng</div>
+              <div className="text-sm py-4">5. Nasi Goreng Seafood</div>
+                </div>
+              <div className="">
+                <Image src="/graph.png" className="w-full" width={300} height={300} />
+              </div>
             </div>
           </div>
-          <div className="w-1/5  bg-blue-500 h-full"></div>
         </div>
       </AdminLayout>
     </div>
